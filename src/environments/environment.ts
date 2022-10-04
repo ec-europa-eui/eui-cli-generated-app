d00194447acb166816feba5924e71b77d400a57f
Nikolaos Bousios <nikolaos.bousios@ext.ec.europa.eu>
2022-10-04 23:51:14 +0200

    fix: config path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment: EuiEnvConfig = {
     production: false,
     enableDevToolRedux: true,
     envDynamicConfig: {
-        uri: 'assets/env-json-config.json',
+        uri: 'assets/config/env-json-config.json',
         deepMerge: true,
         merge: ['modules'],
     },
